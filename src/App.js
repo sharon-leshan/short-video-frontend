4807bd6359a71f89f8e4850fdca415bf0f6582f4
sharon-leshan <maleos380@gmail.com>
2021-10-24 11:01:47 +0300

    short video

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Video from './components/Video';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__videos">
+        <Video
+        url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169738/video1_cvrjfm.mp4"
+        channel="maleos380"
+        description="Mackbook Air to new Linux editing beast"
+        song="I am a Linux PC"
+        likes={345}
+        shares={200}
+        messages={90}
+       /> 
+        <Video url="https://res.cloudinary.com/dxkxvfo2o/video/upload/v1608169739/video2_mecbdo.mp4"
+        channel="maleos380"
+        description="Sunday morning editing on kdenlive in linux"
+        song="Kdenlive is great"
+        likes={445}
+        shares={290}
+        messages={109}
+        />
+      </div>
     </div>
-  );
+      );
 }
 
 export default App;
